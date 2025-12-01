47ad6b32fd68713def4a52659757c2da6ba79e45
Ferraann <ferransansaloni@gmail.com>
2025-12-01 15:28:07 +0100

    avances

--- English_Project_2025/Login.aspx.cs
+++ English_Project_2025/Login.aspx.cs
@@ -13,5 +13,60 @@ namespace English_Project_2025
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string username = TextBox1.Text.Trim();
+            string password = TextBox2.Text.Trim();
+
+            string dbPath = Server.MapPath("~/users.db");
+            string connectionString = $"Data Source={dbPath};Version=3;";
+            string query = "SELECT isDoctor, dni FROM users WHERE username = @Username AND password = @Password";
+
+            try
+            {
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    using (var command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@Password", password);
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                int isDoctor = reader.GetInt32(0);
+                                string dni = reader.GetString(1);
+
+                                Session["username"] = username;
+                                Session["password"] = password;
+                                Session["dni"] = dni;
+
+                                if (isDoctor == 1)
+                                {
+                                    Session["role"] = "Doctor";
+                                    Response.Redirect("~/Doctor.aspx");
+                                }
+                                else
+                                {
+                                    Session["role"] = "Patient";
+                                    Response.Redirect("~/Patient.aspx");
+                                }
+                            }
+                            else
+                            {
+                                LabelMessage.Text = "Invalid username or password.";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LabelMessage.Text = "An error occurred while connecting to the database.";
+            }
+        }
     }
 }
