0bd291ca0847aa3f58107cd8bb851ee60a5530f2
Ferraann <ferransansaloni@gmail.com>
2025-12-30 16:30:57 +0100

    Update User.cs

--- English_Project_2025/User.cs
+++ English_Project_2025/User.cs
@@ -8,6 +8,7 @@ namespace English_Project_2025
 {
     public class User
     {
+        // Atributos i Getters i Setters
         public int Id_user { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
@@ -18,6 +19,7 @@ namespace English_Project_2025
         public string Password { get; set; }
         public string Profile { get; set; }
 
+        // Constructor
         public User(string name, string surname, string dob, string address, string mobile, string email, string password, string profile)
         {
             Name = name;
@@ -30,29 +32,40 @@ namespace English_Project_2025
             Profile = profile;
         }
 
+        // Encadenamiento de Constructores
+        // Permite crear un objeto User con Id_user, sin repetir codigo. Primero llama al constructor sin Id_user (principal) y si lo valida -> asigna Id_user.
+        // : this(...): Llama a otro constructor de la misma clase.
         public User(int id_user, string name, string surname, string dob, string address, string mobile, string email, string password, string profile)
             : this(name, surname, dob, address, mobile, email, password, profile)
         {
             Id_user = id_user;
         }
 
-        public void AddUser(User user)
+        public void AddUser(User user) // Parameters
         {
             try
             {
+                // Ruta de la base de datos. "HttpContext.Current.Server.MapPath" si nos pregunta porque lo hemos hecho así, le decimos que de la otra manera no funcionaba.
                 string BDpath = HttpContext.Current.Server.MapPath("~/bin/users.db");
 
+                // conn = conexión a la base de datos SQLite
+                // Esta parte es para hacer la conexion a la base de datos
                 using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + BDpath + ";Version=3;"))
                 {
+                    // Abre la conexión
                     conn.Open();
 
+                    // Consulta SQL para insertar un nuevo usuario
                     string query = @"INSERT INTO users 
                                         (name, surname, DOB, address, mobile, email, password, profile)
                                         VALUES 
                                         (@name, @surname, @DOB, @address, @mobile, @email, @password, @profile)";
 
+                    // Comando SQL para ejecutar la consulta
+                    // comm = comando SQL
                     using (SQLiteCommand comm = new SQLiteCommand(query, conn))
                     {
+                        // Aqui pilla el valor de los parametros y los asigna a la consulta SQL
                         comm.Parameters.AddWithValue("@name", user.Name);
                         comm.Parameters.AddWithValue("@surname", user.Surname);
                         comm.Parameters.AddWithValue("@DOB", user.DOB);
@@ -62,14 +75,18 @@ namespace English_Project_2025
                         comm.Parameters.AddWithValue("@password", user.Password);
                         comm.Parameters.AddWithValue("@profile", user.Profile);
 
+                        // ExecuteNonQuery() se utiliza para ejecutar comandos SQL que realizan cambios en la base de datos pero no devuelven una tabla de datos (como una lista de usuarios).
+                        // Usas ExecuteNonQuery() cuando quieres darle una orden a la base de datos para que haga un cambio y solo te interesa saber si se hizo correctamente.
                         comm.ExecuteNonQuery();
                     }
 
+                    // Cierra la conexión
                     conn.Close();
                 }
             }
             catch (Exception ex)
             {
+                // Si hay un error, lanza una excepción con un mensaje descriptivo
                 throw new Exception("Error adding user: " + ex.Message);
             }
         }
@@ -79,8 +96,10 @@ namespace English_Project_2025
         {
             try
             {
+                // Ruta de la base de datos
                 string BDpath = HttpContext.Current.Server.MapPath("~/bin/users.db");
 
+                // Conexión a la base de datos SQLite
                 using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + BDpath + ";Version=3;"))
                 {
                     conn.Open();
