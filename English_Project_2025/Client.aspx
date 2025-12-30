7e24632a84dca8d13a2146cfcf958abe284e810b
Ferraann <ferransansaloni@gmail.com>
2025-12-30 17:27:32 +0100

    Cliente comentado

--- English_Project_2025/Client.aspx
+++ English_Project_2025/Client.aspx
@@ -10,23 +10,34 @@
 </head>
 <body>
 
+    <!-- Header -->
 <header>
+    <!-- Enlace que lleva al HomepageLoged y la iamgen de logo. El alt es como una descripcion de la imagen -->
     <a href="HomepageLoged.aspx"> <img src="img/logo.png" alt="Hotel Logo" /> </a>
+    <!-- Nav -->
     <nav>
         <ul>
             <li>
+                <!-- Label donde irá el nombre del usaurio -->
                 <asp:Label ID="LabelName" Text="text" runat="server" />
+                <!-- Icono de logout -->
                 <a href="Logout.aspx" id="logout"> <img src="img/logout.png" alt="Logout icon" /> </a>
             </li>
         </ul>
     </nav>
 </header>
 
+    <!-- Main -->
 <main>  
+    <!-- Cuadrado de la pagina (contenedor) -->
     <div class="content_container">
-        
+        <!-- Título de la pagina -->
         <h2 class="section-title">Personal Data</h2>
-        <div class="info-container">           
+
+        <!-- Contenedor de la informacion del usuario -->
+        <div class="info-container">    
+            <!-- Div con cada informacion del usuario.
+                IMPORTANTE: El label esta vacio pero TIENE id, esa id despues en el c# la usaremos para sustituir el contenido por el dato real del usaurio -->
             <div class="name">
                 <h3>Name:</h3>
                 <asp:Label ID="LabelClientName" Text="i" runat="server"></asp:Label>
@@ -60,15 +71,18 @@
 
         <span class="separator"></span>
        
-        
+        <!-- Título de mis reservas -->
         <h2 class="section-title">My reservations</h2>
         
+        <!-- Div del contenedor de las reservas-->
         <div class="info-container_2">
+            <!-- Este div es donde en c# añadiremos las reservas. A partir del id sabemos donde tienen que ir las reservas -->
             <div id="ReservationsContainer" runat="server"></div>
         </div>
     </div>    
 </main>
 
+    <!-- Header. El &copy; es © -->
 <footer>&copy; All rights reserved | 2025</footer>
 </body>
 </html>
--- English_Project_2025/Client.aspx.cs
+++ English_Project_2025/Client.aspx.cs
@@ -10,8 +10,10 @@ namespace English_Project_2025
 {
     public partial class Cliente : System.Web.UI.Page
     {
+        // Primero que se inicia al cargar la pagina
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Comprobar si la session "Name" existe (si el usuario ha iniciado sesion)
             if (Session["Name"] != null)
             {
                 LabelName.Text = Session["Name"].ToString();
@@ -22,8 +24,10 @@ namespace English_Project_2025
                 LabelClientDOB.Text = Session["DOB"].ToString();
                 LabelClientAddress.Text = Session["Address"].ToString();
 
+                // Si es la primera vez que se carga la pagina hace esto...
                 if (!IsPostBack)
                 {
+                    // ... LoadReservations()
                     LoadReservations();
                 }
             }
@@ -35,22 +39,30 @@ namespace English_Project_2025
 
         private void LoadReservations()
         {
+            // Pilla el id de la sesión
             int userId = Convert.ToInt32(Session["UserID"]);
 
+            // Crea una lista 'reservations' y guarda los datos que recibe del metodo GetReservationsByUser, que le pasa el id recogido de la sesión
             List<Reservation> reservations = Reservation.GetReservationsByUser(userId);
 
+            // Creamos un html
             StringBuilder html = new StringBuilder();
 
+            // Recorre la lista reservations a partir de la variable r. Los datos leídos de reservations, es decir, del reader
             foreach (var r in reservations)
             {
+                // Crea una estructura html, div con una clase y cada dato se pone en forma de <p> y un strong con el titulo de la p.
+                // Usa {r.check_in_date} para recoger y mostrar el dato, en este caso del check-in
                 html.Append("<div class='reservation-card'>");
                 html.Append($"<p><strong>Check-in:</strong> {r.check_in_date}</p>");
                 html.Append($"<p><strong>Check-out:</strong> {r.check_out_date}</p>");
                 html.Append($"<p><strong>Room type:</strong> {r.type_of_room}</p>");
                 html.Append($"<p><strong>Total cost:</strong> {r.total_cost}€</p>");
+                // Despues cierra el div
                 html.Append("</div>");
             }
 
+            // Esto es para cargar este html en el div con id ReservationsContainer
             ReservationsContainer.InnerHtml = html.ToString();
         }
 
